1a754ecf2797ba73f6615303fe0d8b8b8495d008
Gabrieli24 <gabrieli.geronto@gmail.com>
2025-05-13 10:34:34 -0300

    index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head><meta charset="UTF-8"><title>Login</title></head>
+<body>
+<h2>Login</h2>
+<form method="POST" action="login.php">
+  Nome: <input type="text" name="nome"><br>
+  CPF: <input type="text" name="cpf"><br>
+  <button type="submit">Entrar</button>
+</form>
+</body>
+</html>
